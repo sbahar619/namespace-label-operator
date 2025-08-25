dc031f3db5589942354b6aa86d5095d8d096dfca
Shahaf Bahar <sbahar@redhat.com>
2025-08-25 17:19:57 +0300

    Revert "Refactor"
    This reverts commit 2afea42c54a7438d343910941bbb599b92d6fab2.

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -27,6 +27,6 @@ import (
 // Run e2e tests using the Ginkgo runner.
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	_, _ = fmt.Fprintf(GinkgoWriter, "Starting namespacelabel suite\n")
+	fmt.Fprintf(GinkgoWriter, "Starting namespacelabel suite\n")
 	RunSpecs(t, "e2e suite")
 }
